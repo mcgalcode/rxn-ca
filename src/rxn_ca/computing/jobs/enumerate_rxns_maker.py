4bc4a7bd391dec948240f7fc31d9e157203368a5
Max Gallant <mcgallant72@gmail.com>
2023-06-15 10:45:27 -0700

    Remove old enumeration job classes

--- src/rxn_ca/computing/jobs/enumerate_rxns_maker.py
+++ src/rxn_ca/computing/jobs/enumerate_rxns_maker.py
@@ -32,13 +32,28 @@ class EnumerateRxnsMaker(Maker):
             MP_API_KEY=mp_api_key
         )
 
+
+        # The difference between the open enumeration and the basic enumeration is just that another element
+        # is included - you can actually reset the chemical potential _after_ enumeration
+        #
+        # To set a NEW CHEMICAL POTENTIAL - reinitialize a new ReactionSet object with the same underlying
+        # data but with different chempot and open_el values
+        #
+        # To set a new temperature - iterate over every reaction in the set and call get_new_temperature
+        # we will ned up with many more reactions than we had previously, so it is probably good to
+        # filter out reactions with positive delta Gs, say more than 0.1eV per atom
+        #
+        # potential way to use the grand potential enumerator is to run it at a variety of chemical potentials,
+        # then add all the resulting reaction sets to gether, and set the chempot to the desired value for all of them
+        # can calculate oxygen chempots with ktlnPO2 / 2
+
         eset = entry_set_maker.make.original(entry_set_maker, chem_sys)
         enumerator = MinimizeGibbsEnumerator()
         enumeration_maker = ReactionEnumerationMaker()
         enumerators = [enumerator]
         entries = eset.entries
         rxns = enumeration_maker.make.original(enumeration_maker, enumerators, entries)
-        
+
 
         result_model = EnumeratedRxnsModel.from_obj(
             rxns.rxns,
