b1abb38b7d18f6188c79934607751c7fc8b39e72
Max Gallant <mcgallant72@gmail.com>
2024-01-10 11:35:43 -0800

    Fix parameter ordering in utilities

--- src/rxn_ca/utilities/setup_reaction.py
+++ src/rxn_ca/utilities/setup_reaction.py
@@ -1,6 +1,7 @@
 from ..phases.solid_phase_set import SolidPhaseSet
 from ..setup import ReactionPreparer
 
+from pylattica.core import Simulation
 from typing import Dict
 
 def setup_reaction(
@@ -8,16 +9,12 @@ def setup_reaction(
         precursor_mole_ratios: Dict,
         size: int = 15,
         vol_multiplier = 1.0,
-    ):
-
-    num_sites = 500 # min(num_desired_particles, num_allowed_particles)
-    print(f'Nucleating grains at {num_sites} sites')
+    ) -> Simulation:
 
     preparer = ReactionPreparer(phases, dim=3)
     sim = preparer.prepare_reaction(
-        size,
-        num_sites,
         phase_mol_ratios=precursor_mole_ratios,
+        size=size,
         volume_multiplier=vol_multiplier
     )
     
