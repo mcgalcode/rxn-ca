98d8b7e721a701ac706443ed9af14b5099c68088
Max Gallant <mcgallant72@gmail.com>
2024-01-10 12:29:38 -0800

    Add test for parallel reaction runner

--- tests/integration/test_basic_reaction.py
+++ tests/integration/test_basic_reaction.py
@@ -24,4 +24,23 @@ def test_basic_reaction(get_test_file_path):
     assert "BaTi2O5" in product_phases
     assert "Ba4Ti13O30" in product_phases
 
+def test_parallel_rxn(get_test_file_path):
+    recipe = ReactionRecipe.from_file(get_test_file_path("integration/batio3_recipe.json"))
+    rxn_lib = ReactionLibrary.from_file(get_test_file_path("integration/batio3_library.json"))
+
+    result_doc = run_sim_parallel(recipe, reaction_lib=rxn_lib)
+
+    result_analyzer = BulkReactionAnalyzer(results=result_doc.results,
+                                           phase_set=result_doc.reaction_library.phases,
+                                           heating_sched=result_doc.recipe.heating_schedule)
+
+    step_analyzer = ReactionStepAnalyzer(rxn_lib.phases)
+
+    product_phases = step_analyzer.phases_present(result_analyzer.get_final_steps())
+
+    assert "BaTiO3" in product_phases
+    assert "Ba2TiO4" in product_phases
+    assert "BaTi2O5" in product_phases
+    assert "Ba4Ti13O30" in product_phases
+
     
